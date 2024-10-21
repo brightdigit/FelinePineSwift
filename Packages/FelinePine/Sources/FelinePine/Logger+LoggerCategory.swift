a7c5232ebc67b005e7273d834cdf1d2b9c2b3434
Leo Dion <leogdion@brightdigit.com>
2024-10-21 16:48:07 -0400

    git subrepo clone --branch=v1.0.0-beta.5 git@github.com:brightdigit/FelinePine.git Packages/FelinePine
    subrepo:
      subdir:   "Packages/FelinePine"
      merged:   "0609809"
    upstream:
      origin:   "git@github.com:brightdigit/FelinePine.git"
      branch:   "v1.0.0-beta.5"
      commit:   "0609809"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/Homebrew/brew"
      commit:   "42c11a4ded"

---
+++ Packages/FelinePine/Sources/FelinePine/Logger+LoggerCategory.swift
@@ -0,0 +1,56 @@
+//
+//  Logger+LoggerCategory.swift
+//  FelinePine
+//
+//  Created by Leo Dion.
+//  Copyright © 2024 BrightDigit.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the “Software”), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#if swift(<6.0)
+  #if canImport(os)
+    import os
+
+  #endif
+#else
+  #if canImport(os)
+    public import os
+
+  #endif
+#endif
+
+#if canImport(os) 
+  extension Logger {
+    internal init<Category: RawRepresentable>(
+      subsystem: String,
+      category: Category
+    ) where Category.RawValue == String {
+      #if canImport(os)
+        self.init(subsystem: subsystem, category: category.rawValue)
+      #else
+        self.init(label: subsystem)
+        self[metadataKey: "category"] = "\(category)"
+      #endif
+    }
+  }
+#endif
