0f436a6826bec6e317e01cd8255478d25cc3f10f
Leo Dion <leogdion@brightdigit.com>
2025-01-13 21:18:58 -0500

    updating to FelinePine 1.0.0

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/brightdigit/FelinePine.git", from: "1.0.0-beta.5"),
+    .package(url: "https://github.com/brightdigit/FelinePine.git", from: "1.0.0"),
     .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0")
   ],
   targets: [
